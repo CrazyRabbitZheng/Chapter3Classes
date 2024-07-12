3ccd2d879ceb41a94af309713e679e7ca69108ef
CrazyRabbitZheng <alicejrha@gmail.com>
2024-07-12 00:40:24 -0230

    update

--- src/AccountTest.java
+++ src/AccountTest.java
@@ -9,8 +9,6 @@ public class AccountTest {
        Account account1 = new Account("Jane Mary", 50.00);
        Account account2 = new Account("Harry Potter", -7.53);
 
-       System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
-       System.out.printf("%s balance: $%.2f%n", account2.getName(), account2.getBalance());
 
        Scanner input = new Scanner(System.in);
 
@@ -19,22 +17,25 @@ public class AccountTest {
        System.out.printf("Adding %.2f to account1 balance: %n", depositAmount1);
        account1.deposit(depositAmount1);
 
-       System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
-       System.out.printf("%s balance: $%.2f%n", account2.getName(), account2.getBalance());
+
 
        System.out.print("Enter deposit amount for account 2: ");
        double depositAmount2 = input.nextDouble();
        System.out.printf("Adding %.2f to account2 balance: %n", depositAmount2);
        account2.deposit(depositAmount2);
 
-       System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
-       System.out.printf("%s balance: $%.2f%n", account2.getName(), account2.getBalance());
 
        System.out.println("How much would you like to withdraw?");
        double withdrawAmount1 = input.nextDouble();
        account1.withdraw(withdrawAmount1);
 
-       System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
 
+       displayAccount(account1);
+       displayAccount(account2);
+
+    }
+
+    public static void displayAccount(Account account) {
+       System.out.printf("%s balance: $%.2f%n", account.getName(), account.getBalance());
     }
 }
